536ef6134fec454b35ba5ab49925deb102b84bbb
Erick Dourado <erickdourado_@hotmail.com>
2024-09-25 21:51:21 -0300

    Start project

---
+++ src/test/java/com/canto/paladar/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.canto.paladar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
