d52c9c152b05814ce839b83b86a50d9ac8243826
Erick Dourado <erickdourado_@hotmail.com>
2024-09-26 23:19:38 -0300

    Create exception and global handler

---
+++ src/main/java/com/canto/paladar/exception/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.canto.paladar.exception;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import static org.springframework.http.HttpStatus.CONFLICT;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException ex) {
+        return ResponseEntity.status(NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<String> handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
+        return ResponseEntity.status(CONFLICT).body(ex.getMessage());
+    }
+
+}
