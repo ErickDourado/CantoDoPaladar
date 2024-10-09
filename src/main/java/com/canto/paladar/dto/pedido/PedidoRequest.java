4a83addec7f13cda8e21a15fb75ec0cd0d922df1
Erick Dourado <erickdourado_@hotmail.com>
2024-10-08 23:23:11 -0300

    Adjust pedido request and response

--- src/main/java/com/canto/paladar/dto/pedido/PedidoRequest.java
+++ src/main/java/com/canto/paladar/dto/pedido/PedidoRequest.java
@@ -1,10 +1,13 @@
 package com.canto.paladar.dto.pedido;
 
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.List;
 
 public record PedidoRequest(
         @PositiveOrZero(message = "Subtotal não pode ser negativo")
@@ -17,7 +20,15 @@ public record PedidoRequest(
 
         @PositiveOrZero(message = "Total não pode ser negativo")
         @NotNull(message = "Total não pode ser nulo")
-        Double total
+        Double total,
+
+        @Positive(message = "Id do endereço deve ser maior que zero")
+        @NotNull(message = "Id do endereço não pode ser nulo")
+        Long enderecoId,
+
+        @NotEmpty(message = "Lista de produtos não pode ser vazia")
+        @NotNull(message = "Lista de produtos não pode ser nula")
+        List<Long> produtosId
 ) implements Serializable {
 
     @Serial
--- src/main/java/com/canto/paladar/dto/pedido/PedidoResponse.java
+++ src/main/java/com/canto/paladar/dto/pedido/PedidoResponse.java
@@ -1,13 +1,19 @@
 package com.canto.paladar.dto.pedido;
 
+import com.canto.paladar.dto.endereco.EnderecoResponse;
+import com.canto.paladar.dto.produto.ProdutoResponse;
+
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.Set;
 
 public record PedidoResponse(
         Long id,
         Double subtotal,
         Double frete,
-        Double total
+        Double total,
+        EnderecoResponse endereco,
+        Set<ProdutoResponse> produtos
 ) implements Serializable {
 
     @Serial
